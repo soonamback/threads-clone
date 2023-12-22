8cf6529f823f9917ed0d4f7d05ce97200f7fd429
soonamback <soonam.yi123@gmail.com>
2023-12-22 21:26:35 +0900

    add first layout

---
+++ components/Post.vue
@@ -0,0 +1,46 @@
+<template>
+ <div class="z-50 bottom-0 h-full w-full">
+        <div class=" py-2 w-full">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center text-white">
+                    <img class="rounded-full h-[35px]" :src="post.image">
+                    <div class="ml-2 font-semibold text-[18px]">{{ post.name }}</div>
+                </div>
+                <div @click="isMenu = !isMenu" class="relative">
+                    <button 
+                        :disabled="isDeleting"
+                        class="flex items-center text-white p-1 h-[24px] w-[24px] hover:bg-gray-800 rounded-full cursor-pointer" 
+                        :class="isMenu ? 'bg-gray-800' : ''"
+                    >
+                        <Icon v-if="!isDeleting" name="bi:three-dots" color="#ffffff" size="18"/>
+                        <Icon v-else name="eos-icons:bubble-loading" color="#ffffff" size="18"/>
+                    </button>
+
+                    <div v-if="isMenu" class="absolute border border-gray-600 right-0 z-20 mt-1 rounded">
+                        <button class="flex items-center rounded gap-2 text-red-500 justify-between bg-black w-full pl-4 pr-3 py-1 hover:bg-gray-900"
+                        >
+                            <div>Delete</div>
+                            <Icon name="solar:trash-bin-trash-broken" size="20"/>
+                        </button>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+</template>
+
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+
+const runtimeConfig = useRuntimeConfig()
+let isMenu = ref(false)
+let isLike = ref(false)
+let isDeleting = ref(false)
+
+const emit = defineEmits(['isDeleted'])
+const props = defineProps({ post: Object })
+
+// const client = useSupabaseClient()
+// const user = useSupabaseUser()
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,36 @@
 <template>
-    <div>
-        Index
-    </div>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto" >
+                    <div v-if="isPosts" v-for="post in posts" :key="post">
+                        <Post :post="post" @isDeleted="posts = []" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
 
 </template>
 
 <script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+// const user = useSupabaseUser()
 
+let posts = ref([]);
+let isPosts = ref(true)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+    posts.value = [
+        {
+            name: 'Soonam',
+            image: 'https://placehold.co/100',
+            text: 'this is a title',
+            picture: 'https://placehold.co/500',
+        }
+    ]
+})
 </script>
