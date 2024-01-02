e2ee72f25d5946f3ce4d879c15f5a4448dac3dec
soonamback <soonam.yi123@gmail.com>
2024-01-02 16:19:13 +0900

    modifies app.vue

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
+  <VitePwaManifest />
+
   <div class="bg-black fixed w-[calc(100%+1px)] h-full">
     <NuxtPage />
 
@@ -8,10 +10,17 @@
           {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
       ]"
     />
+
+    <Modal 
+      :class="[
+          {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isLogoutOverlay },
+          {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isLogoutOverlay },
+      ]"
+    />
   </div>
 </template>
 
 <script setup>
-import { useUserStore  } from '~/stores/user';
+import { useUserStore } from '~/stores/user';
 const userStore = useUserStore()
 </script>
